4ae36550986f9937143e32985462912c402459b8
Furkan Kuyumcu <fkuyumcu@k1m30s01.42istanbul.com.tr>
2025-02-15 13:41:21 +0300

    lex

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: fkuyumcu <fkuyumcu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 13:59:10 by fkuyumcu          #+#    #+#             */
-/*   Updated: 2025/02/14 17:07:12 by fkuyumcu         ###   ########.fr       */
+/*   Updated: 2025/02/15 13:40:36 by fkuyumcu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <unistd.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
 
 # define RST   "\x1b[0m"
 # define BLACK   "\x1b[30m"
@@ -31,11 +35,15 @@
 
 typedef enum
 {
-    TOKEN_WORD,
-    TOKEN_PIPE, 
+    TOKEN_PIPE,
     TOKEN_REDIRECT_IN,
     TOKEN_REDIRECT_OUT,
+    TOKEN_REDIRECT_APPEND_IN,
+    TOKEN_REDIRECT_APPEND_OUT,
     TOKEN_AMPERSAND,
+    TOKEN_WORD,
+    TOKEN_STRING,
+    TOKEN_ENV_VAR,
     TOKEN_END
 } token_type;
 
@@ -45,13 +53,21 @@ typedef struct token_s
     char value[100];
 } token_t;
 
+typedef struct minishell_s
+{
+	int	single_quote_flag;
+	int double_quote_flag;
+}
+minishell_t;
 
 void    ft_getcwd(char *buf, size_t size);
 char    *ft_readline(void);
-void    lex_analize(char *input, token_t tokens[]);
+void    lex_analize(char *input, token_t tokens[], minishell_t *minishell);
 void	print_banner(void);
 void	parser(char *buf);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
 size_t	ft_strlen(const char *s);
+char    *format_cwd(const char *cwd);
+
 
 #endif
--- src/parser.c
+++ src/parser.c
@@ -6,14 +6,107 @@
 /*   By: fkuyumcu <fkuyumcu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 19:32:13 by fkuyumcu          #+#    #+#             */
-/*   Updated: 2025/02/14 16:43:26 by fkuyumcu         ###   ########.fr       */
+/*   Updated: 2025/02/15 13:39:32 by fkuyumcu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 
-void lex_analize(char *input, token_t tokens[])
+void quote_checker(minishell_t *a, char *str)
+{
+    int i = 0;
+    a->double_quote_flag = 0;
+    a->single_quote_flag = 0;
+    while (str[i])
+    {
+        if (str[i] == '"' || str[i] == '\'')
+        {
+            if (str[i] == '"' && a->single_quote_flag % 2 == 0)
+            {
+                a->double_quote_flag++;
+            }
+            else if (str[i] == '\'' && a->double_quote_flag % 2 == 0)
+            {
+                a->single_quote_flag++;
+            }
+        }
+        i++;
+    }
+    if (a->double_quote_flag % 2 != 0 || a->single_quote_flag % 2 != 0)
+        write(1, "error\n", 6);
+}
+
+void redirect_append_out(char **input, token_t tokens[], int *count)
+{
+    //TODO
+}
+
+
+void redirect_append_in(char **input, token_t tokens[], int *count)
+{
+    //TODO    
+}
+
+void proc_env(char **input, token_t tokens[], int *count)
+{
+    //TODO
+}
+
+void double_quote(char **input, token_t tokens[], int *count, minishell_t *minishell)
+{
+    quote_checker(minishell, *input);
+}
+
+
+void single_quote(char **input, token_t tokens[], int *count, minishell_t *minishell)
+{
+    quote_checker(minishell, *input);
+    
+}
+
+void process_word(char **input, token_t tokens[], int *count)
+{
+    char *start = *input;
+    while (**input != '\0' && !isspace(**input) && **input != '|' &&
+           **input != '<' && **input != '>' && **input != '&' && **input != '\'' && **input != '"')
+        (*input)++;
+
+    int length = *input - start;
+    if (length >= MAX_TOKEN_LEN) 
+        length = MAX_TOKEN_LEN - 1;
+
+    ft_strncpy(tokens[*count].value, start, length);
+    tokens[*count].value[length] = '\0';
+    tokens[*count].t_type = TOKEN_WORD;
+    (*count)++;
+}
+
+void process_token(char **input, token_t tokens[], int *count, minishell_t *minishell)
+{
+    if (**input == '|')
+        tokens[(*count)++] = (token_t){TOKEN_PIPE, '|'};
+    else if (**input == '<' && *(*input + 1) == '<')
+        tokens[(*count)++] = (token_t){TOKEN_REDIRECT_APPEND_IN, '<<'};
+    else if (**input == '>' && *(*input + 1) == '>')
+        tokens[(*count)++] = (token_t){TOKEN_REDIRECT_APPEND_OUT, '>>'};
+    else if (**input == '<')
+        tokens[(*count)++] = (token_t){TOKEN_REDIRECT_IN, '<'};
+    else if (**input == '>')
+        tokens[(*count)++] = (token_t){TOKEN_REDIRECT_OUT, '>'};
+    else if (**input == '&')
+        tokens[(*count)++] = (token_t){TOKEN_AMPERSAND, '&'};
+    else if (**input == '\"')
+        double_quote(input, tokens, count, minishell);
+    else if (**input == '\'')
+        single_quote(input, tokens, count, minishell);
+    else if (**input == '$')
+        proc_env(input, tokens, count);
+    else 
+        process_word(input, tokens, count);
+}
+
+void lex_analize(char *input, token_t tokens[], minishell_t *minishell)
 {
     int count = 0;
 
@@ -23,48 +116,18 @@ void lex_analize(char *input, token_t tokens[])
             input++;
         if (*input == '\0')
             break;
-        if (*input == '|')
-        {
-            tokens[count++] = (token_t){TOKEN_PIPE, '|'};
-            input++;
-        }
-        else if(*input == '<')
-        {
-            tokens[count++] = (token_t){TOKEN_REDIRECT_IN, '<'};
-            input++;
-        }
-        else if(*input == '>')
-        {
-            tokens[count++] = (token_t){TOKEN_REDIRECT_OUT, '>'};
-            input++;
-        }
-        else if(*input == '&')
-        {
-            tokens[count++] = (token_t){TOKEN_AMPERSAND, '&'};
-            input++;
-        }
-        else 
-        {
-            char *start = input;
-            while (*input != '\0' && !isspace(*input) && *input != '|' &&
-                   *input != '<' && *input != '>' && *input != '&')
-                input++;
-            int length = input - start;
-            if (length >= MAX_TOKEN_LEN) 
-                length = MAX_TOKEN_LEN - 1;
-            ft_strncpy(tokens[count].value, start, length);
-            tokens[count].value[length] = '\0';
-            tokens[count].t_type = TOKEN_WORD;
-            count++;
-        }
+        process_token(&input, tokens, &count, minishell);
     }
     tokens[count] =  (token_t){TOKEN_END, ""};
 }
 
 void parser(char *buf)
 {
-    token_t tokens[MAX_TOKEN_LEN];
-    lex_analize(buf, tokens);
+    minishell_t *minishell;
 
+    if(!(minishell = malloc(sizeof(minishell_t))))
+        perror("Error");
     
+    token_t tokens[MAX_TOKEN_LEN];
+    lex_analize(buf, tokens, minishell);
 }
