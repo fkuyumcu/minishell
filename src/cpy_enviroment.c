406ac4650bd0ceba46b545a2721ac6facddd34a2
yunuseus <134627365+yunuseus@users.noreply.github.com>
2025-02-21 20:00:22 +0300

    Add files via upload

---
+++ src/cpy_enviroment.c
@@ -0,0 +1,19 @@
+#include "minishell.h"
+
+void	cpy_enviroment(char **env, minishell_t *minishell)
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+		i++;
+	minishell->env = malloc(sizeof(char *) * (i + 1));
+	i = 0;
+	while (env[i])
+	{
+		minishell->env[i] = strdup(env[i]);
+		i++;
+	}
+	minishell->env[i] = NULL;
+	
+}
