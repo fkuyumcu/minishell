9b0b65796de7eb6991325f19a2b51927d22eefc9
yunuseus <134627365+yunuseus@users.noreply.github.com>
2025-02-22 17:56:42 +0300

    Update echo.c

--- builtin/echo.c
+++ builtin/echo.c
@@ -1,31 +1,49 @@
-
-
-
-
-
-
-void	echo_flag(char **arg)
+int	is_flag_n(char *arg)
 {
-	int	f;
 	int	i;
 
-	f = 0;
+	i = 1;
+	if (arg[0] != '-' || arg[1] != 'n')
+		return (0);
+	while (arg[i])
+	{
+		if (arg[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	flag_ctrl(char **arg, int *i)
+{
+	if (is_flag_n(arg[*i]) == 1)
+		*i += 1;
+}
+
+void	echo_with_arg(char **arg)
+{
+	int	i;
+	int	flag;
+
+	flag = 0;
 	i = 1;
 	if (arg[i] && arg[i][0] == '-')
 		flag_ctrl(arg, &i);
-	f = i;
-	while	
+	while (arg[i])
+	{
+		printf("%s", arg[i]);
+		if (arg[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (is_flag_n(arg[1]) == 0)
+		printf("\n");
 }
 
-
-
-
-
-
-void echo(char **arg)
+void	echo(char **arg)
 {
 	if (arg && arg[1])
-		echo_flag(arg);
+		echo_with_arg(arg);
 	else
 		printf("\n");
 }
