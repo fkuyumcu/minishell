7b2c36e5621f9d9947be999d16c821d549267f3e
Furkan Kuyumcu <furkankuyumcu2004@gmail.com>
2025-03-17 13:45:55 +0300

    Update exec_word.c

--- src/exec/exec_word.c
+++ src/exec/exec_word.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_word.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yalp <yalp@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: fkuyumcu <fkuyumcu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 16:18:07 by fkuyumcu          #+#    #+#             */
-/*   Updated: 2025/03/15 14:04:07 by yalp             ###   ########.fr       */
+/*   Updated: 2025/03/17 13:28:38 by fkuyumcu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 void free_str_array(char **paths, int i)
 {
-	if (paths[i]
+	if (paths[i])
 	{
 		while (paths[++i])
 			free(paths[i]);
@@ -31,11 +31,10 @@ char *find(char *cmd, char **envp, minishell_t *minishell)
     int i = 0;
     char *part_path;
 
-    // PATH değişkeninin olup olmadığını kontrol et
     while (envp[i] && ft_strnstr(envp[i], "PATH", 4) == 0)
         i++;
     if (!envp[i])
-        return NULL;  // PATH değişkeni yoksa NULL dön
+        return NULL;
 
     paths = ft_split(envp[i] + 5, ':');
     if (!paths)
@@ -47,11 +46,12 @@ char *find(char *cmd, char **envp, minishell_t *minishell)
         if (!part_path)
             break;
         path = ft_strjoin(part_path, cmd ,minishell);
+        //free(part_path);
         if (!path)
             break;
-		if (access(path, F_OK) != 0) // Dosya mevcutsa
+		if (access(path, F_OK) != 0)
 			free(path);
-        else if (access(path, F_OK) == 0) // Dosya mevcutsa
+        else if (access(path, F_OK) == 0)
         {
 			free(paths);
 			free_str_array(paths, i);
@@ -61,7 +61,7 @@ char *find(char *cmd, char **envp, minishell_t *minishell)
     }
 	free_str_array(paths, i);
  	free(paths);
-    return NULL;
+    return (NULL);
 }
 
 
@@ -74,9 +74,7 @@ void	executed(char *argv, char **envp, minishell_t *ms)
 	path = find(command[0], envp, ms);
 	if (path == 0)
 	{
-		//liberte(command);
         printf("Error");
-		//error(ERR_CMD);
 	}
 	if (execve(path, command, envp) == -1)
 		printf("Error");
@@ -87,43 +85,28 @@ void execute_word(ast_node_t *node, minishell_t *ms)
 {
         char *cmd = node->args[0];
         char *exec_path = NULL;
-
+        int status;
+        pid_t pid;
         if (access(cmd, X_OK) == 0)
             exec_path = cmd;
+            
         else
             exec_path = find(cmd, ms->envp, ms);
-
-        if (!exec_path)
-        {
-            fprintf(stderr, "Error: Command not found: %s\n", cmd);
-            exit(EXIT_FAILURE);
-        }
-
-        pid_t pid = fork();
-        if (pid == -1)
-        {
-            perror("fork failed");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0) // Çocuk işlem
-        {
             
-            execve(exec_path, node->args, ms->envp);
-            perror("execve failed"); 
-            exit(EXIT_FAILURE);
-        }
-        else // Parent process
+        if (!exec_path)
+            printf("Error: Command not found: %s\n", cmd);//error
+
+        pid = fork();
+        if (pid == -1)
+           printf("error");//err
+        else if (pid == 0)   
         {
-            int status;
+            execve(exec_path, node->args, ms->envp);
+            free(exec_path);
+        }
+        else
+        {
 			free(exec_path);
             waitpid(pid, &status, 0);
-            if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-            {
-                fprintf(stderr, "Error: Command execution failed: %s\n", cmd);
-                exit(EXIT_FAILURE);
-            }
         }
 }
-
-
-
