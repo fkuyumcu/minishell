c676bd819ce506f50c8b877c6c3279929148e102
Furkan Kuyumcu <fkuyumcu@k1m17s05.42istanbul.com.tr>
2025-05-09 11:12:42 +0300

    $

--- src/parser/env_check.c
+++ src/parser/env_check.c
@@ -31,13 +31,18 @@ static void	proc_env(t_token *token, t_minishell *ms)
 {
 	char	*dolar_pos;
 	int		len;
+ 	char	*exit_code;
 
 	len = 0;
 	dolar_pos = ft_strchr(token->value, '$');
 	if (dolar_pos && *(dolar_pos + 1) != '?')
 		replace_env_value(token, ms, dolar_pos, len);
 	if (ft_strnstr(token->value, "$?", ft_strlen(token->value)))
-		change_dollar_ask(token, ft_itoa(g_code % 256));
+	{
+		exit_code = ft_itoa(g_code % 256);
+		change_dollar_ask(token, exit_code);
+		free(exit_code);
+	}
 }
 
 void	check_env(t_token tk[], t_minishell *minishell)
